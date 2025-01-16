df9d5c63ae1c62a7faee0840b2d2c6da8a9fa613
Björn Ström <bjornstrom90@gmail.com>
2025-01-16 10:27:59 +0100

    commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import streamlit as st
 
+
+rho_api_key = st.secrets.get("rho_api_key")
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.write("rho_api_key: {}".format(rho_api_key))
+st.write("asdasd")
+
